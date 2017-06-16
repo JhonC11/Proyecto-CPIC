20c3001b2a591d0ca8d4e2576f0731f5357c7842
paolasanchezq <cpsanchez37@misena.edu.co>
2017-06-16 14:30:56 -0500

    paola

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -11,7 +11,7 @@
 			'https://unsplash.it/660/455?image=838'
 			],
 			align: true,
-			getViewAllText: function(imgsCount) { return 'View all' }
+			getViewAllText: function(imgsCount) { return 'Ver más' }
 		});
 
 	});
